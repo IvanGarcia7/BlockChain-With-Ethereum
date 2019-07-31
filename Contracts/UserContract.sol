505f3f34403d43f72f5730ab2519aba97c92672e
Ivan <ivan@MacBook-Pro-de-Ivan.local>
2019-07-31 15:22:29 +0200

    New Version

--- Contracts/UserContract.sol
+++ Contracts/UserContract.sol
@@ -5,10 +5,23 @@ contract UserContract {
     //Creacion de estructura de usuario
     struct User {
         string name;
-        string apellido;
+        string surname;
     }
 
     //Para cada dirección representaremos un usuario
     mapping(address => User) private usuarios;
+
+    //funcion para registrarse en el contrato
+    function join(string name,string surname) public {
+        User storage userActual = usuarios[msg.sender];
+        userActual.name = name;
+        userActual.surname = surname;
+    }
+
+    //funcion para recuperar nombre y apellido usuario
+    function getUser(address addr) public view returns(string,string) {
+        User memory userActual = usuarios[addr];
+        return (userActual.name,userActual.surname);
+    }
 
 }
