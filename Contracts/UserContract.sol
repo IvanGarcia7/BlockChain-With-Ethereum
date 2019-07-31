33b67020142f8ecb349a32ea96cba7ee0f8823a0
Ivan <ivan@MacBook-Pro-de-Ivan.local>
2019-07-31 15:43:37 +0200

    Añadidas nuevas funciones al contrato
    Se han añadido nuevas funciones y establecido una serie de restricciones

--- Contracts/UserContract.sol
+++ Contracts/UserContract.sol
@@ -10,18 +10,28 @@ contract UserContract {
 
     //Para cada dirección representaremos un usuario
     mapping(address => User) private usuarios;
+    //Mapping de tipo direcciona  bool
+    mapping(address => bool) private usuariosRegistrados;
 
     //funcion para registrarse en el contrato
     function join(string name,string surname) public {
+        require(!userJoined(msg.sender),"Address already registered");
         User storage userActual = usuarios[msg.sender];
         userActual.name = name;
         userActual.surname = surname;
+        usuariosRegistrados[msg.sender] = true;
     }
 
     //funcion para recuperar nombre y apellido usuario
     function getUser(address addr) public view returns(string,string) {
+        require(userJoined(msg.sender),"Unregistered Address");
         User memory userActual = usuarios[addr];
         return (userActual.name,userActual.surname);
     }
+
+    function userJoined(address addr) private view returns(bool) {
+        return usuariosRegistrados[addr];
+    }
+
 
 }
